6cbe1d758c1f0b94f108f6de85658a13dc38e895
Peter Siemens <peterjsiemens@gmail.com>
2020-04-08 18:00:24 -0700

    Remove UpdateAccountCode helper

--- blockchain.go
+++ blockchain.go
@@ -613,38 +613,6 @@ func (b *Blockchain) CreateAccount(publicKeys []flow.AccountKey, code []byte) (f
 	return b.LastCreatedAccount().Address, nil
 }
 
-// UpdateAccountCode submits a transaction to update the code of an existing account
-// with the given code. The transaction is paid by the root account.
-func (b *Blockchain) UpdateAccountCode(code []byte) error {
-	updateAccountScript := templates.UpdateAccountCode(code)
-
-	rootKey := b.RootKey()
-
-	tx := flow.NewTransaction().
-		SetScript(updateAccountScript).
-		SetGasLimit(10).
-		SetProposalKey(rootKey.Address, rootKey.ID, rootKey.SequenceNumber).
-		SetPayer(rootKey.Address, rootKey.ID).
-		AddAuthorizer(rootKey.Address, rootKey.ID)
-
-	err := tx.SignContainer(rootKey.Address, rootKey.ID, rootKey.Signer())
-	if err != nil {
-		return err
-	}
-
-	err = b.AddTransaction(*tx)
-	if err != nil {
-		return err
-	}
-
-	_, _, err = b.ExecuteAndCommitBlock()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // verifyAccountSignature verifies that an account signature is valid for the
 // account and given message.
 //
