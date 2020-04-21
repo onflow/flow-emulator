99d5f46d797d1d961bd2592795cd3a2bbdbb17fc
Peter Siemens <peterjsiemens@gmail.com>
2020-04-21 07:29:57 -0700

    Allow default root key to be configured

--- cmd/emulator/main.go
+++ cmd/emulator/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"github.com/onflow/flow-go-sdk/crypto"
+
+	emulator "github.com/dapperlabs/flow-emulator"
 	"github.com/dapperlabs/flow-emulator/cmd/emulator/start"
 )
 
+const DefaultRootPrivateKeySeed = emulator.DefaultRootPrivateKeySeed
+
+func defaultRootKey() (crypto.PrivateKey, crypto.SignatureAlgorithm, crypto.HashAlgorithm) {
+	rootPrivateKey, _ := crypto.GeneratePrivateKey(crypto.ECDSA_P256, []byte(DefaultRootPrivateKeySeed))
+
+	rootKeySigAlgo := rootPrivateKey.Algorithm()
+	rootKeyHashAlgo := crypto.SHA3_256
+
+	return rootPrivateKey, rootKeySigAlgo, rootKeyHashAlgo
+}
+
 func main() {
-	if err := start.Cmd.Execute(); err != nil {
+	if err := start.Cmd(defaultRootKey).Execute(); err != nil {
 		start.Exit(1, err.Error())
 	}
 }
