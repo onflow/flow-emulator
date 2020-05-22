9356d30a6ed14ebbfb5529faecb8113ff9f8e3ed
Peter Siemens <peterjsiemens@gmail.com>
2020-05-22 10:47:04 -0700

    Update tests to use new bootstrapped state

--- utils/unittest/assertions.go
+++ utils/unittest/assertions.go
@@ -4,15 +4,20 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/dapperlabs/flow-emulator/types"
 )
 
-func AssertFlowVMErrorType(t *testing.T, err error, expected interface{}) bool {
-
-	assert.IsType(t, err, &types.FlowError{})
+func AssertFlowVMErrorType(t *testing.T, expected interface{}, err error) bool {
+	require.IsType(t, err, &types.FlowError{})
 
 	flowError := err.(*types.FlowError)
 
-	return assert.IsType(t, flowError.FlowError, expected)
+	if !assert.IsType(t, expected, flowError.FlowError) {
+		t.Log(flowError.FlowError.ErrorMessage())
+		return false
+	}
+
+	return true
 }
