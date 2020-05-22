b3c034e39f9a2ab880b76756be581582026831ac
Peter Siemens <peterjsiemens@gmail.com>
2020-05-22 15:04:02 -0700

    Add emulator.MaxGasLimit constant

--- init_test.go
+++ init_test.go
@@ -69,7 +69,7 @@ func TestInitialization(t *testing.T) {
 
 		tx := flow.NewTransaction().
 			SetScript([]byte(script)).
-			SetGasLimit(defaultGasLimit).
+			SetGasLimit(emulator.MaxGasLimit).
 			SetProposalKey(b.RootKey().Address, b.RootKey().ID, b.RootKey().SequenceNumber).
 			SetPayer(b.RootKey().Address).
 			AddAuthorizer(b.RootKey().Address)
