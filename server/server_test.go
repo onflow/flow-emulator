2673873c638d9d180e1c3a44e29af5e079476940
Bastian Müller <bastian@axiomzen.co>
2022-06-14 13:47:00 -0700

    update NFTStorefront contract, test

---
+++ server/server_test.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExecuteScript(t *testing.T) {
+
+	t.Parallel()
+
+	server := NewEmulatorServer(logrus.New(), &Config{})
+
+	const code = `
+      pub fun main(): String {
+	      return "Hello"
+      }
+    `
+	result, err := server.backend.ExecuteScriptAtLatestBlock(context.Background(), []byte(code), nil)
+	require.NoError(t, err)
+
+	require.JSONEq(t, `{"type":"String","value":"Hello"}`, string(result))
+
+}
+
+func TestExecuteScriptImportingContracts(t *testing.T) {
+
+	t.Parallel()
+
+	conf := &Config{
+		WithContracts: true,
+	}
+
+	server := NewEmulatorServer(logrus.New(), conf)
+
+	serviceAccount := server.blockchain.ServiceKey().Address.Hex()
+
+	code := fmt.Sprintf(
+		`
+	      import ExampleNFT, NFTStorefront from 0x%s
+
+          pub fun main() {
+		      let collection <- ExampleNFT.createEmptyCollection()
+		      destroy collection
+
+		      NFTStorefront
+		  }
+        `,
+		serviceAccount,
+	)
+
+	_, err := server.backend.ExecuteScriptAtLatestBlock(context.Background(), []byte(code), nil)
+	require.NoError(t, err)
+
+}
