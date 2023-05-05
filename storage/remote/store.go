feb906c6548a904d1fc0b72dcca934184a725c62
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2023-05-05 19:21:21 +0200

    add remote ledger

--- storage/remote/store.go
+++ storage/remote/store.go
@@ -2,9 +2,15 @@ package remote
 
 import (
 	"context"
+	"fmt"
 	"github.com/onflow/flow-archive/api/archive"
 	"github.com/onflow/flow-emulator/storage"
+	exeState "github.com/onflow/flow-go/engine/execution/state"
 	"github.com/onflow/flow-go/fvm/state"
+	"github.com/onflow/flow-go/fvm/storage/snapshot"
+	"github.com/onflow/flow-go/ledger/common/pathfinder"
+	"github.com/onflow/flow-go/ledger/complete"
+	flowgo "github.com/onflow/flow-go/model/flow"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -33,5 +39,25 @@ func (s *Store) LedgerByHeight(
 	ctx context.Context,
 	blockHeight uint64,
 ) state.StorageSnapshot {
+
+	snapshot.NewReadFuncStorageSnapshot(func(id flowgo.RegisterID) (flowgo.RegisterValue, error) {
+		ledgerKey := exeState.RegisterIDToKey(flowgo.RegisterID{Key: id.Key, Owner: id.Owner})
+		ledgerPath, err := pathfinder.KeyToPath(ledgerKey, complete.DefaultPathFinderVersion)
+
+		response, err := s.client.GetRegisterValues(ctx, &archive.GetRegisterValuesRequest{
+			Height: blockHeight,
+			Paths:  [][]byte{ledgerPath[:]},
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		if len(response.Values) == 0 {
+			return nil, fmt.Errorf("not found value for register id %s", id.String())
+		}
+
+		return response.Values[0], nil
+	})
+
 	return nil
 }
