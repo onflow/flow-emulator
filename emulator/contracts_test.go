45f68788e08f9a643ccf974f0f660ec341f0a593
Deniz Mert Edincik <deniz@edincik.com>
2023-06-21 18:01:54 +0200

    better test

--- emulator/contracts_test.go
+++ emulator/contracts_test.go
@@ -5,9 +5,7 @@ import (
 	flowgo "github.com/onflow/flow-go/model/flow"
 	"testing"
 
-	"github.com/onflow/cadence"
 	"github.com/onflow/flow-emulator/emulator"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -17,10 +15,18 @@ func TestCommonContractsDeployment(t *testing.T) {
 
 	//only test monotonic and emulator ( mainnet / testnet is used for remote debugging )
 	chains := []flowgo.Chain{
-		flowgo.MonotonicEmulator.Chain(),
 		flowgo.Emulator.Chain(),
+		flowgo.MonotonicEmulator.Chain(),
 	}
 
+	contracts := []string{
+		"FUSD",
+		"MetadataViews",
+		"ExampleNFT",
+		"NFTStorefrontV2",
+		"NFTStorefront",
+		"NonFungibleToken",
+	}
 	for _, chain := range chains {
 
 		b, err := emulator.New(
@@ -30,41 +36,18 @@ func TestCommonContractsDeployment(t *testing.T) {
 		require.NoError(t, err)
 
 		serviceAccount := b.ServiceKey().Address.Hex()
-		scriptCode := fmt.Sprintf(`
-	    import
-	        FUSD,
-	        NonFungibleToken,
-	        MetadataViews,
-	        ExampleNFT,
-	        NFTStorefrontV2,
-	        NFTStorefront
-	    from 0x%s
 
-	    pub fun main(): Bool {
-	        log(Type<FUSD>().identifier)
-	        log(Type<NonFungibleToken>().identifier)
-			log(Type<MetadataViews>().identifier)
-	        log(Type<ExampleNFT>().identifier)
-	        log(Type<NFTStorefrontV2>().identifier)
-	        log(Type<NFTStorefront>().identifier)
-	        return true
-	    }
-	`, serviceAccount)
+		for _, contract := range contracts {
 
-		scriptResult, err := b.ExecuteScript([]byte(scriptCode), [][]byte{})
-		require.NoError(t, err)
-		assert.ElementsMatch(
-			t,
-			[]string{
-				fmt.Sprintf("\"A.%s.FUSD\"", serviceAccount),
-				fmt.Sprintf("\"A.%s.NonFungibleToken\"", serviceAccount),
-				fmt.Sprintf("\"A.%s.MetadataViews\"", serviceAccount),
-				fmt.Sprintf("\"A.%s.ExampleNFT\"", serviceAccount),
-				fmt.Sprintf("\"A.%s.NFTStorefrontV2\"", serviceAccount),
-				fmt.Sprintf("\"A.%s.NFTStorefront\"", serviceAccount),
-			},
-			scriptResult.Logs,
-		)
-		assert.Equal(t, cadence.NewBool(true), scriptResult.Value)
+			scriptCode := fmt.Sprintf(`
+			pub fun main() {
+				getAccount(0x%s).contracts.get(name: "%s") ?? panic("contract is not deployed")
+	    	}`, serviceAccount, contract)
+
+			scriptResult, err := b.ExecuteScript([]byte(scriptCode), [][]byte{})
+			require.NoError(t, err)
+			require.NoError(t, scriptResult.Error)
+
+		}
 	}
 }
