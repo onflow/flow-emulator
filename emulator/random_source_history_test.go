214b6530d486d4f10b49be28982a76b4d1dde853
Jordan Schalm <jordan@dapperlabs.com>
2023-12-05 16:52:53 -0500

    Update emulator/random_source_history_test.go
    Co-authored-by: Bastian Müller <bastian@turbolent.com>

--- emulator/random_source_history_test.go
+++ emulator/random_source_history_test.go
@@ -81,7 +81,7 @@ func TestRandomSourceHistoryAtBlockHeight(t *testing.T) {
         access(all)
         fun main(): Bool {
             let sorAt1 = RandomBeaconHistory.sourceOfRandomness(atBlockHeight: 1)
-			let sorAt2 = RandomBeaconHistory.sourceOfRandomness(atBlockHeight: 2)
+            let sorAt2 = RandomBeaconHistory.sourceOfRandomness(atBlockHeight: 2)
 
             assert(sorAt1.blockHeight == 1)
             assert(sorAt2.blockHeight == 2)
