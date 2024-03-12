038714573ae3582b556955c699c8dfa798a9f9cd
Ardit Marku <markoupetr@gmail.com>
2024-03-12 12:06:10 +0200

    Add config flags to enable/disable reporting of computation profiles

--- server/server.go
+++ server/server.go
@@ -142,6 +142,8 @@ type Config struct {
 	RPCHost string
 	// StartBlockHeight is the height at which to start the emulator.
 	StartBlockHeight uint64
+	// ComputationProfilingEnabled enables/disables Cadence computation profiling.
+	ComputationProfilingEnabled bool
 }
 
 type listener interface {
@@ -431,6 +433,13 @@ func configureBlockchain(logger *zerolog.Logger, conf *Config, store storage.Sto
 		options = append(
 			options,
 			emulator.WithLegacyUpgradeEnabled(),
+		)
+	}
+
+	if conf.ComputationProfilingEnabled {
+		options = append(
+			options,
+			emulator.WithComputationProfiling(true),
 		)
 	}
 
