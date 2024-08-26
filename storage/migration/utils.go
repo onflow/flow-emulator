4ba5bee33ac03b18794e423b123b134faa83bfcb
Bastian Müller <bastian@turbolent.com>
2024-08-26 10:10:12 -0700

    update copyright notice

--- storage/migration/utils.go
+++ storage/migration/utils.go
@@ -1,7 +1,7 @@
 /*
  * Flow Emulator
  *
- * Copyright 2024 Dapper Labs, Inc.
+ * Copyright Flow Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- storage/util/defaultStore_wasm.go
+++ storage/util/defaultStore_wasm.go
@@ -4,7 +4,7 @@
 /*
  * Flow Emulator
  *
- * Copyright 2019 Dapper Labs, Inc.
+ * Copyright Flow Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
