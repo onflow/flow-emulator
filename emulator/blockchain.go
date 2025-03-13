d6606be93530d01c5fa5e7e2e7fb468e9cb6e8c6
Peter Argue <89119817+peterargue@users.noreply.github.com>
2025-03-13 07:32:09 -0700

    Fix getting accounts

--- emulator/blockchain.go
+++ emulator/blockchain.go
@@ -1092,13 +1092,7 @@ func (b *Blockchain) getAccount(address flowgo.Address) (*flowgo.Account, error)
 func (b *Blockchain) GetAccountAtBlockHeight(address flowgo.Address, blockHeight uint64) (*flowgo.Account, error) {
 	b.mu.RLock()
 	defer b.mu.RUnlock()
-
-	account, err := b.getAccountAtBlock(address, blockHeight)
-	if err != nil {
-		return nil, err
-	}
-
-	return account, nil
+	return b.getAccountAtBlock(address, blockHeight)
 }
 
 // GetAccountAtBlock returns the account for the given address at specified block height.
@@ -1113,7 +1107,7 @@ func (b *Blockchain) getAccountAtBlock(address flowgo.Address, blockHeight uint6
 		return nil, &types.AccountNotFoundError{Address: address}
 	}
 
-	return account, nil
+	return account, err
 }
 
 func (b *Blockchain) GetEventsForBlockIDs(eventType string, blockIDs []flowgo.Identifier) (result []flowgo.BlockEvents, err error) {
