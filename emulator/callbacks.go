1c1a3f2840a34b5ea5024419be8e17c24d8633d0
gregor <gregor.gololicic93@gmail.com>
2025-05-23 19:52:49 +0200

    add callbacks logic for parsing events and preparing transactions

--- emulator/callbacks.go
+++ emulator/callbacks.go
@@ -4,6 +4,8 @@ import (
 	_ "embed"
 	"encoding/binary"
 	"fmt"
+	"strings"
+
 	"github.com/onflow/cadence"
 	jsoncdc "github.com/onflow/cadence/encoding/json"
 	flowsdk "github.com/onflow/flow-go-sdk"
@@ -13,26 +15,41 @@ import (
 //go:embed templates/executeCallbackTransaction.cdc
 var executeCallbackScript []byte
 
-//go:embed templates/scheduleCallbackTransaction.cdc
-var scheduleCallbackScript []byte
+//go:embed templates/processCallbackTransaction.cdc
+var processCallbackScript []byte
 
-func scheduleCallbackTransaction() flow.TransactionBody {
+func processCallbackTransaction(
+	serviceAddress flow.Address,
+	parentID flow.Identifier,
+) flow.TransactionBody {
+	script := replaceSchedulerAddress(processCallbackScript, serviceAddress)
+
 	return *flow.NewTransactionBody().
-		SetScript(scheduleCallbackScript)
+		SetScript(script).
+		SetComputeLimit(defaultTransactionMaxGasLimit).
+		SetPayer(serviceAddress).
+		SetReferenceBlockID(parentID)
 }
 
-func executeCallbackTransactions(scheduleEvent []flowsdk.Event) ([]flow.TransactionBody, error) {
+func executeCallbackTransactions(
+	scheduleEvent []flowsdk.Event,
+	serviceAddress flow.Address,
+	parentID flow.Identifier,
+) ([]flow.TransactionBody, error) {
 	var transactions []flow.TransactionBody
+	script := replaceSchedulerAddress(executeCallbackScript, serviceAddress)
 
 	for _, e := range scheduleEvent {
-		limit, id, err := parseSchedulerProcessedEvent(e)
+		limit, id, err := parseSchedulerProcessedEvent(e, serviceAddress)
 		if err != nil {
 			return nil, err
 		}
 
 		tx := flow.NewTransactionBody().
-			SetScript(executeCallbackScript).
+			SetScript(script).
 			AddArgument(id).
+			SetPayer(serviceAddress).
+			SetReferenceBlockID(parentID).
 			SetComputeLimit(limit)
 
 		transactions = append(transactions, *tx)
@@ -47,22 +64,45 @@ func executeCallbackTransactions(scheduleEvent []flowsdk.Event) ([]flow.Transact
 // - execution effort
 // - ID of the event encoded as bytes
 // - error in case the event type is not correct
-func parseSchedulerProcessedEvent(event flowsdk.Event) (uint64, []byte, error) {
-	if event.Type != "A.Scheduler.CallbackProcessed" {
-		return 0, nil, fmt.Errorf("invalid event type: %s", event.Type)
+func parseSchedulerProcessedEvent(event flowsdk.Event, serviceAddress flow.Address) (uint64, []byte, error) {
+	callbackScheduledEvent := fmt.Sprintf(
+		"A.%s.UnsafeCallbackScheduler.CallbackProcessed",
+		serviceAddress.String(),
+	)
+	const (
+		IDField        = "id"
+		executionField = "executionEffort"
+	)
+
+	if event.Type != callbackScheduledEvent {
+		return 0, nil, fmt.Errorf("invalid event type, got: %s, expected: %s", event.Type, callbackScheduledEvent)
 	}
 
-	id := event.Value.SearchFieldByName("ID")
+	id, ok := event.Value.SearchFieldByName(IDField).(cadence.UInt64)
+	if !ok {
+		return 0, nil, fmt.Errorf("invalid ID value type: %v", id)
+	}
+
 	encodedID, err := jsoncdc.Encode(id)
 	if err != nil {
 		return 0, nil, err
 	}
 
-	effort, ok := event.Value.SearchFieldByName("computationEffort").(cadence.UInt64)
+	effort, ok := event.Value.SearchFieldByName(executionField).(cadence.UInt64)
 	if !ok {
 		return 0, nil, fmt.Errorf("invalid effort value type: %v", effort)
 	}
 	computeLimit := binary.BigEndian.Uint64(effort.ToBigEndianBytes())
 
 	return computeLimit, encodedID, nil
+}
+
+func replaceSchedulerAddress(script []byte, serviceAddress flow.Address) []byte {
+	s := strings.ReplaceAll(
+		string(script),
+		`import "UnsafeCallbackScheduler"`,
+		fmt.Sprintf("import UnsafeCallbackScheduler from %s", serviceAddress.HexWithPrefix()),
+	)
+
+	return []byte(s)
 }
