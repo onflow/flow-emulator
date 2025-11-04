620875f0164740b55f0d72eec755bdf42584662b
Bastian Müller <bastian@turbolent.com>
2025-11-04 11:12:07 -0800

    add test

---
+++ emulator/computation_profile_test.go
@@ -0,0 +1,97 @@
+/*
+ * Flow Emulator
+ *
+ * Copyright Flow Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package emulator_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/onflow/cadence"
+	"github.com/onflow/cadence/common"
+	"github.com/onflow/cadence/runtime"
+	flowsdk "github.com/onflow/flow-go-sdk"
+	"github.com/onflow/flow-go/fvm/meter"
+	flowgo "github.com/onflow/flow-go/model/flow"
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/onflow/flow-emulator/adapters"
+	"github.com/onflow/flow-emulator/emulator"
+)
+
+func TestComputationProfile(t *testing.T) {
+
+	t.Parallel()
+
+	computationProfile := runtime.NewComputationProfile()
+	b, err := emulator.New(
+		emulator.WithComputationProfile(computationProfile),
+		emulator.WithExecutionEffortWeights(
+			meter.ExecutionEffortWeights{
+				common.ComputationKindFunctionInvocation: 1,
+				common.ComputationKindStatement:          1,
+				common.ComputationKindLoop:               1,
+			},
+		),
+	)
+	require.NoError(t, err)
+
+	computationProfile.Reset()
+
+	logger := zerolog.Nop()
+	adapter := adapters.NewSDKAdapter(&logger, b)
+
+	addTwoScript, counterAddress := DeployAndGenerateAddTwoScript(t, adapter)
+
+	tx := flowsdk.NewTransaction().
+		SetScript([]byte(addTwoScript)).
+		SetComputeLimit(flowgo.DefaultMaxTransactionGasLimit).
+		SetProposalKey(b.ServiceKey().Address, b.ServiceKey().Index, b.ServiceKey().SequenceNumber).
+		SetPayer(b.ServiceKey().Address).
+		AddAuthorizer(b.ServiceKey().Address)
+
+	signer, err := b.ServiceKey().Signer()
+	require.NoError(t, err)
+
+	err = tx.SignEnvelope(b.ServiceKey().Address, b.ServiceKey().Index, signer)
+	require.NoError(t, err)
+
+	callScript := GenerateGetCounterCountScript(counterAddress, b.ServiceKey().Address)
+
+	// Sample call (value is 0)
+	scriptResult, err := b.ExecuteScript([]byte(callScript), nil)
+	require.NoError(t, err)
+	assert.Equal(t, cadence.NewInt(0), scriptResult.Value)
+
+	// Submit tx (script adds 2)
+	err = adapter.SendTransaction(context.Background(), *tx)
+	require.NoError(t, err)
+
+	txResult, err := b.ExecuteNextTransaction()
+	require.NoError(t, err)
+	AssertTransactionSucceeded(t, txResult)
+
+	pprofProfile, err := runtime.NewPProfExporter(computationProfile).Export()
+	require.NoError(t, err)
+	require.NotNil(t, pprofProfile)
+
+	require.NotEmpty(t, pprofProfile.Function)
+	require.NotEmpty(t, pprofProfile.Sample)
+}
